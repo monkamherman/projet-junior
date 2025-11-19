751f30527fac8db5efcc0b4fed1b54336a0985b3
monkamherman <cesaristos5@gmail.com>
2025-11-19 23:22:14 +0100

    ouf

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animate from 'tailwindcss-animate';
 export default {
   mode: 'jit',
   darkMode: ['class'],
@@ -127,5 +128,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [animate],
 };
